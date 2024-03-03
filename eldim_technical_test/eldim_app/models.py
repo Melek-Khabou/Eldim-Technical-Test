33fadcda6eff3b41a9293132248ff9d51d18272a
Melek-khabou <melek.khabou@enis.tn>
2024-03-03 14:21:14 +0100

    feat: create employee model

--- eldim_technical_test/eldim_app/models.py
+++ eldim_technical_test/eldim_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.TextField()
+    office = models.CharField(max_length=100)
+    age = models.IntegerField()
+    startDate = models.DateField()
+    salary = models.DecimalField(max_digits=20, decimal_places=3)
